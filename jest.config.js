4fef53fe1a2facd63d79ff70109c65fdcb5cf753
6-ON <aminhatim6@gmail.com>
2023-12-17 22:18:07 +0100

    Update moduleNameMapper in jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,7 @@
-/* eslint-disable no-undef */
 module.exports = {
 	preset: "ts-jest",
 	testEnvironment: "node",
 	moduleNameMapper: {
-		"@app/(.*)": "<rootDir>/src/$1",
+		"@/(.*)": "<rootDir>/src/$1",
 	},
 };
