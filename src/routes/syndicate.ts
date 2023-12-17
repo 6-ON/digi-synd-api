dc5488b1cbb4adfccba074cf46e6816c8cff7608
6-ON <aminhatim6@gmail.com>
2023-12-17 22:28:29 +0100

    apply verifyJWT and requireRoles middlewares to syndicate route

--- src/routes/syndicate.ts
+++ src/routes/syndicate.ts
@@ -1,8 +1,11 @@
 import { SyndicateController } from "@/controllers";
+import { Roles } from "@/interfaces/models";
+import { requireRoles, verifyJwt } from "@/middlewares";
 import { Router } from "express";
 
 const router = Router();
-
-router.post("/",SyndicateController.create);
+router.use(verifyJwt);
+router.use(requireRoles([Roles.ADMIN]));
+router.post("/", SyndicateController.create);
 
 export default router;
