c7ab9ed07c3e325c791711917c139dbeb440d967
6-ON <aminhatim6@gmail.com>
2023-12-17 22:15:29 +0100

    Add flattenObject example

--- src/utils/objects.ts
+++ src/utils/objects.ts
@@ -1,6 +1,23 @@
 /**
  * @description 
  * @returns single level object
+ * @example 
+ * const obj = {
+ * 	foo: {
+ * 		bar: "baz",
+ * 		qux: "quux",
+ * 	},
+ * 	quux: {
+ * 		corge: "grault",
+ * 	},
+ * };
+ * const flatObj = flattenObject(obj);
+ * console.log(flatObj);
+ * // {
+ * // 	"foo.bar": "baz",
+ * // 	"foo.qux": "quux",
+ * // 	"quux.corge": "grault",
+ * // }
  */
 export function flattenObject(obj:any, prefix = "") {
 	const flatObject = {};
