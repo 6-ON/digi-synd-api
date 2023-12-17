1936187e1531379723b8f27cd8597f54b4db6d20
6-ON <aminhatim6@gmail.com>
2023-12-17 15:02:57 +0100

    move reflect-metadata to config

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,5 @@
-import "reflect-metadata";
-
-import express, { Application } from "express";
 import "./config";
+import express, { Application } from "express";
 import router from "./routes";
 import morgan from "morgan";
 import { handleErrors } from "./middlewares";
--- src/config/index.ts
+++ src/config/index.ts
@@ -1,2 +1,3 @@
+import "reflect-metadata"
 import "dotenv/config"; // sets up process.env from .env file
 import "express-async-errors"; // handles async errors
