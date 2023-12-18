f2adab17b8008670766edc3e44d23058de7c49b6
6-ON <aminhatim6@gmail.com>
2023-12-18 10:12:41 +0100

    Add requireOwner middleware to factures route

--- src/routes/factures.ts
+++ src/routes/factures.ts
@@ -1,14 +1,13 @@
 import { FactureController } from "@/controllers";
 import { Router } from "express";
 import { apartmentParam } from "./params";
-import { verifyJwt } from "@/middlewares";
+import { requireOwner, verifyJwt } from "@/middlewares";
 
 const router = Router();
 
 router.param("apartment", apartmentParam);
-router.use(verifyJwt)
-router.get("/",FactureController.recent);
-router.post("/:apartment",FactureController.create);
-
+router.use(verifyJwt);
+router.get("/", FactureController.recent);
+router.post("/:apartment", requireOwner, FactureController.create);
 
 export default router;
