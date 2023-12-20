f969eaf866f0237d1e5de20d82f528e6dd7d2aca
6-ON <aminhatim6@gmail.com>
2023-12-20 11:16:55 +0100

    Refactor create-admin script to improve readability

--- src/scripts/create-admin.ts
+++ src/scripts/create-admin.ts
@@ -1,16 +1,12 @@
-import { connectToDb } from "@/db";
 import { Roles } from "@/interfaces/models";
 import { UserService } from "@/services";
 
 export default async function run() {
-	const user = await UserService.create(
-		{
-			email: `admin${new Date().getTime()}@digi.com`,
-			password: "admin",
-			name: "admin",
-		},
-		Roles.ADMIN,
-	);
-	// log only email and password
-	console.table([user.email, "admin"], ["email", "password"]);
+	const admin = {
+		email: `admin${new Date().getTime()}@digi.com`,
+		password: "admin",
+		name: "admin",
+	};
+	const user = await UserService.create(admin, Roles.ADMIN);
+	console.table([admin]);
 }
